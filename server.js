9a678c2bc697bf9ea8a469020e7cc9678ee37a77
Bibek-codes <bibekpanda07@gmail.com>
2022-11-03 13:02:10 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb+srv://Bibek_codes:class_mate07*@cluster0.w0t2o.mongodb.net/shop')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(3000)
